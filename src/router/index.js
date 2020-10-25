1a1810bdb17af00c967e8a38a7e3b90bad4d92e7
wangkaiwd <1162486176@qq.com>
2020-10-25 16:38:05 +0800

    feat: supply router navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -15,14 +15,24 @@ const routes = [
     name: 'About',
     component: About,
     children: [
-      { path: 'a', name: 'AboutA', component: { render: (h) => <div>This is about/a</div> } },
-      { path: 'b', name: 'AboutB', component: { render: (h) => <div>This is about/b</div> } }
+      { path: 'a', name: 'AboutA', component: { name: 'AboutA', render: (h) => <div>This is about/a</div> } },
+      { path: 'b', name: 'AboutB', component: { name: 'AboutB', render: (h) => <div>This is about/b</div> } }
     ]
   }
 ];
 
 const router = new VueRouter({
   routes
+});
+router.beforeEach((to, from, next) => {
+  console.log(1);
+  setTimeout(() => {
+    next();
+  }, 1000);
+});
+router.beforeEach((to, from, next) => {
+  console.log(2);
+  next();
 });
 
 export default router;
